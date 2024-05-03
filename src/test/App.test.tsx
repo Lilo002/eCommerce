f9ff6f8474758db5cd2cc6e4e98f79711ca3af17
CrazyTapok <masyanya0772@gmail.com>
2024-05-03 23:43:40 +0300

    fix: change  simple-import-sort/imports

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,3 +1,19 @@
+import { render, screen } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+
+import App from '../App';
+
 test('demo', () => {
   expect(true).toBe(true);
+});
+
+test('Renders the main page', () => {
+  render(<App />);
+  expect(true).toBeTruthy();
+});
+
+test('Should render headline', () => {
+  render(<App />);
+  expect(screen.getByText('Hello World!!!')).toBeInTheDocument();
 });
