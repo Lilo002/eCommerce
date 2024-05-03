4310c4b6c4d727bab96797257e010f2f878cc071
Lilo002 <lisabasarab@gmail.com>
2024-05-03 15:20:46 +0300

    feat: add session context

---
+++ src/context/sessionContext.ts
@@ -0,0 +1,13 @@
+import { Project } from "@commercetools/platform-sdk";
+import { createContext } from "react";
+
+// export const anonymousContext = createContext<{user: Project | null}>({user: null})
+export const sessionContext = createContext<{session: {
+  isAuth: boolean;
+  auth: Project | null;
+  login: ({ email, password }: {
+      email: string;
+      password: string;
+  }) => void;
+  logout: () => void;
+}  | null}>({session: null})
