1d6e4089147d52a677693d8ed448118256def481
Lilo002 <lisabasarab@gmail.com>
2024-05-03 15:27:58 +0300

    feat: provide session context with useSession value to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { RouterProvider } from "react-router-dom"
 import "./styles/_app.scss"
-import { router } from "./router"
+import { useSession } from "./hooks/useSession";
+import { sessionContext } from "./context/sessionContext";
+import { router } from "./router";
 
 function App() {
+  const session = useSession();
+
   return (
+  <sessionContext.Provider value={{session}}>
     <RouterProvider router={router}/>
+  </sessionContext.Provider>
   )
 }
 
