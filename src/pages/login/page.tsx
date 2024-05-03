eea9431475b7724ae80556796b9761fdd25dd20a
Lilo002 <lisabasarab@gmail.com>
2024-05-03 15:22:09 +0300

    feat: add form and method to submit values

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom"
 import { ROUTES } from "../../shared/constants"
+import { FormEvent, useContext, useState } from "react"
 
 export const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onEmailChange = (value: string) => {
+    setEmail(value);
+  }
+
+  const onPasswordChange = (value: string) => {
+    setPassword(value);
+  }
+
+  const onFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }
+
   return <div>
     <h1>Login page</h1>
     <button>
@@ -10,5 +26,10 @@ export const LoginPage = () => {
     <button>
       <Link to={ROUTES.REGISTRATION}>to Registration</Link>
     </button>
+    <form onSubmit={(e) => onFormSubmit(e)}>
+      <input type="text" placeholder="email" name="email" value={email} onChange={(e) => onEmailChange(e.target.value)}/>
+      <input type="text" placeholder="password" name='password' value={password} onChange={(e) => onPasswordChange(e.target.value)}/>
+      <button type="submit">login</button>
+    </form>
   </div>
 }
