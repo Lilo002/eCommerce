c30ff352660308f2b55565732152319992fd743a
Dmitry Markovich <44719809+CrazyTapok@users.noreply.github.com>
2024-05-04 12:14:01 +0300

    Feat/8694fvpam (#6)
    * feat: install and configure jest, add test for App component, create a script for running Jest tests
    
    * feat: install and configure jest, add test for App component, create a script for running Jest tests
    
    * docs: expand readme description
    
    * fix: change  simple-import-sort/imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import sass from 'sass';
-// import vitePluginSass from 'vite-plugin-sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
