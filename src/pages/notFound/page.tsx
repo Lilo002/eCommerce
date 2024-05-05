3f632ccfe1a7dde0a88f5744a0af3d68e304b60e
CrazyTapok <masyanya0772@gmail.com>
2024-05-05 23:16:43 +0300

    feat: add page 404 antd library

--- src/pages/notFound/page.tsx
+++ src/pages/notFound/page.tsx
@@ -1,3 +1,19 @@
+import { Link } from 'react-router-dom';
+import { Button, Result } from 'antd';
+
+import { ROUTES } from '../../shared/constants';
+
 export function NotFoundPage() {
-  return <div>Not Found</div>;
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary">
+          <Link to={ROUTES.MAIN}>Back Main page</Link>
+        </Button>
+      }
+    />
+  );
 }
