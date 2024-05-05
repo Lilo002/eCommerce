b1ed1e4fb5b8f5413a84ad4796509d640c0b9a0a
Lilo002 <lisabasarab@gmail.com>
2024-05-05 17:55:03 +0300

    feat: add more style to form

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -2,11 +2,11 @@ import { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button, Form, Input, message } from 'antd';
 
+import { Header } from '../../components/header/header';
 import { sessionContext } from '../../context/sessionContext';
 import { ROUTES } from '../../shared/constants';
 
 import './_page.scss';
-import { Header } from '../../components/header/header';
 
 const emailRules = [
   { required: true, message: 'Please input your email' },
@@ -58,10 +58,11 @@ export function LoginPage() {
       <Header />
       <Form
         labelCol={{ span: 4 }}
-        wrapperCol={{ span: 16 }}
+        wrapperCol={{ offset: 0, span: 24 }}
         className="login-form"
         onFinish={onFormSubmit}
         autoComplete="off"
+        layout="vertical"
       >
         <div className="login-info">
           <span className="login-title">Sign In</span>
@@ -69,19 +70,25 @@ export function LoginPage() {
             New to Lidilu? Create new account
           </Link>
         </div>
-        <Form.Item name="email" label="Email" rules={emailRules} validateFirst hasFeedback>
-          <Input value={email} onChange={(e) => onEmailChange(e.target.value)} />
-        </Form.Item>
+        <div className="login-content">
+          <Form.Item name="email" label="Email" rules={emailRules} validateFirst hasFeedback>
+            <Input value={email} onChange={(e) => onEmailChange(e.target.value)} style={{ width: '100%' }} />
+          </Form.Item>
 
-        <Form.Item name="password" label="Password" rules={passwordRules} validateFirst hasFeedback>
-          <Input.Password value={password} onChange={(e) => onPasswordChange(e.target.value)} />
-        </Form.Item>
+          <Form.Item name="password" label="Password" rules={passwordRules} validateFirst hasFeedback>
+            <Input.Password
+              value={password}
+              onChange={(e) => onPasswordChange(e.target.value)}
+              style={{ width: '100%' }}
+            />
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            SIGN IN
-          </Button>
-        </Form.Item>
+          <Form.Item wrapperCol={{ offset: 0, span: 16 }}>
+            <Button type="primary" htmlType="submit" style={{ backgroundColor: '#f47721'}}>
+              SIGN IN
+            </Button>
+          </Form.Item>
+        </div>
       </Form>
     </>
   );
