c2ce59f0e493698f0b9692be4f08d6059132dc95
CrazyTapok <masyanya0772@gmail.com>
2024-05-05 23:45:14 +0300

    feat: add rule for displaying the button Back Main page and Back Log in page

--- src/pages/notFound/page.tsx
+++ src/pages/notFound/page.tsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Result } from 'antd';
 
+import { sessionContext } from '../../context/sessionContext';
 import { ROUTES } from '../../shared/constants';
 
 import './UI/_notFound.scss';
 
 export function NotFoundPage() {
+  const { session } = useContext(sessionContext);
+
   return (
     <div className="not-found-container">
       <Result
@@ -13,9 +17,15 @@ export function NotFoundPage() {
         title="404"
         subTitle="Sorry, the page you visited does not exist."
         extra={
-          <Button type="primary">
-            <Link to={ROUTES.MAIN}>Back Main page</Link>
-          </Button>
+          session?.isLogin ? (
+            <Button type="primary">
+              <Link to={ROUTES.MAIN}>Back Main page</Link>
+            </Button>
+          ) : (
+            <Button type="primary">
+              <Link to={ROUTES.LOGIN}>Back Log in page</Link>
+            </Button>
+          )
         }
       />
     </div>
