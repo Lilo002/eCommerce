709a8d97abcaf497a5db51d7cd34471a39ea3448
CrazyTapok <masyanya0772@gmail.com>
2024-05-05 23:25:37 +0300

    feat: add additional style to page 404

--- src/pages/notFound/page.tsx
+++ src/pages/notFound/page.tsx
@@ -3,17 +3,21 @@ import { Button, Result } from 'antd';
 
 import { ROUTES } from '../../shared/constants';
 
+import './UI/_notFound.scss';
+
 export function NotFoundPage() {
   return (
-    <Result
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
-      extra={
-        <Button type="primary">
-          <Link to={ROUTES.MAIN}>Back Main page</Link>
-        </Button>
-      }
-    />
+    <div className="not-found-container">
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary">
+            <Link to={ROUTES.MAIN}>Back Main page</Link>
+          </Button>
+        }
+      />
+    </div>
   );
 }
