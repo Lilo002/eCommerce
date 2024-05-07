2e0078513e5c8e471e5d4192d6f080caefb15977
CrazyTapok <masyanya0772@gmail.com>
2024-05-07 12:51:28 +0300

    fix: change structure NotFoundPage component

--- src/pages/notFound/page.tsx
+++ src/pages/notFound/page.tsx
@@ -1,15 +1,11 @@
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Result } from 'antd';
 
-import { sessionContext } from '../../context/sessionContext';
 import { ROUTES } from '../../shared/constants';
 
 import './UI/_notFound.scss';
 
 export function NotFoundPage() {
-  const { session } = useContext(sessionContext);
-
   return (
     <div className="not-found-container">
       <Result
@@ -17,15 +13,9 @@ export function NotFoundPage() {
         title="404"
         subTitle="Sorry, the page you visited does not exist."
         extra={
-          session?.isLogin ? (
-            <Button type="primary">
-              <Link to={ROUTES.MAIN}>Back Main page</Link>
-            </Button>
-          ) : (
-            <Button type="primary">
-              <Link to={ROUTES.LOGIN}>Back Log in page</Link>
-            </Button>
-          )
+          <Button type="primary">
+            <Link to={ROUTES.MAIN}>Back Main page</Link>
+          </Button>
         }
       />
     </div>
