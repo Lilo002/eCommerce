de6a54136713880ebdfeb50dff2198bb4d036eba
Luba Hadunova <luba.godunova2018@gmail.com>
2024-05-07 07:18:51 +0200

    feat: add registration form with validation

---
+++ src/pages/registration/model/validation.ts
@@ -0,0 +1,87 @@
+export const emailRules = [
+  { required: true, message: 'Please input your email' },
+  { pattern: /^\S+$/, message: 'Email must not contain leading or trailing whitespace' },
+  { pattern: /^[\w.]+@[\w.]+\.\w+$/, message: 'Invalid email format' },
+];
+
+export const passwordRules = [
+  { required: true, message: 'Please input your password' },
+  { min: 8, message: 'Password must be at least 8 characters' },
+  { pattern: /^\S+$/, message: 'Password must not contain leading or trailing whitespace' },
+  {
+    pattern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]?)[A-Za-z\d!@#$%^&*]+$/,
+    message: 'Password must contain uppercase letters, lowercase letters, digits and special characters',
+  },
+];
+
+export function textRules(field: string) {
+  return [
+    { required: true, message: `Please input your ${field}` },
+    {
+      pattern: /^[a-zA-Z]+$/,
+      message: `${field} must contain at least one character and no special characters or numbers`,
+    },
+  ];
+}
+
+const validateAge = (value: string): boolean => {
+  const validAge = 13;
+  const birthdayDate = new Date(value);
+  const currentDate = new Date();
+  const currentDateToValidAge = new Date(
+    currentDate.getFullYear() - validAge,
+    currentDate.getMonth(),
+    currentDate.getDate(),
+  );
+  if (birthdayDate > currentDateToValidAge) {
+    return false;
+  }
+  return true;
+};
+
+export const ageRules = [
+  { required: true, message: 'Please input your date of birth' },
+  {
+    validator: (_: object, value: string) => {
+      if (validateAge(value)) {
+        return Promise.resolve();
+      }
+      return Promise.reject(new Error('You should have 13 years old or older'));
+    },
+  },
+];
+
+export const countryRules = [{ required: true, message: `Please select your country` }];
+
+// const validatePostalCode = (value: string): boolean => {
+//   const validAge = 13;
+//   const birthdayDate = new Date(value);
+//   const currentDate = new Date();
+//   const currentDateToValidAge = new Date(
+//     currentDate.getFullYear() - validAge,
+//     currentDate.getMonth(),
+//     currentDate.getDate(),
+//   );
+//   if (birthdayDate > currentDateToValidAge) {
+//     return false;
+//   }
+//   return true;
+// };
+
+export const postalCodeRules = [
+  { required: true, message: 'Please input your postal code' },
+  // { pattern: /^\d{2}-\d{3}/, message: 'Must be a valid postal code' },
+  //   {
+  //     validator: (_: object, value: string) => {
+  //       if (validateAge(value)) {
+  //         return Promise.resolve();
+  //       }
+  //       return Promise.reject(new Error('You should have 13 years old or older'));
+  //     },
+  //   },
+];
+
+export const streetRules = [
+  { required: true, message: 'Please input your street' },
+  { pattern: /^\w+$/, message: 'Street must contain at least one character' },
+];
--- src/pages/registration/page.tsx
+++ src/pages/registration/page.tsx
@@ -1,17 +1,119 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, DatePicker, Form, Input, Select } from 'antd';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import MaskedInput from 'antd-mask-input';
 
 import { ROUTES } from '../../shared/constants';
 
+import * as validation from './model/validation';
+
+import './_page.scss';
+
+const countryMasks = {
+  Belarus: '00-00-00',
+  Canada: '00-00-00',
+  Poland: '00-000',
+  US: '00-000',
+};
+
 export function RegistrationPage() {
+  const [countryValue, setCountryValue] = useState('Poland');
+  const [postalCodeMask, setPostalCodeMask] = useState('00-000');
+  const [postalCodeValue, setPostalCodeValue] = useState('00-000');
+
+  const changeCountry = (value: string) => {
+    setCountryValue(value);
+    const currentMask = countryMasks[value as keyof typeof countryMasks];
+    setPostalCodeMask(currentMask);
+    setPostalCodeValue('');
+  };
+
   return (
-    <div>
-      <h1>Registration page</h1>
-      <button type="button">
-        <Link to={ROUTES.MAIN}>to main</Link>
-      </button>
-      <button type="button">
-        <Link to={ROUTES.LOGIN}>to login</Link>
-      </button>
-    </div>
+    <Form
+      labelCol={{ span: 4 }}
+      wrapperCol={{ offset: 0, span: 24 }}
+      className="registration-form"
+      autoComplete="off"
+      layout="vertical"
+    >
+      <div className="registration-info">
+        <span className="registration-title">Registration</span>
+        <Link to={ROUTES.LOGIN} className="registration-login">
+          Already have an account? Sign In
+        </Link>
+      </div>
+      <div className="registration-content">
+        <Form.Item
+          name="firstName"
+          label="First name:"
+          rules={validation.textRules('First name')}
+          validateFirst
+          hasFeedback
+        >
+          <Input style={{ width: '100%' }} />
+        </Form.Item>
+
+        <Form.Item
+          name="lastName"
+          label="Last Name"
+          rules={validation.textRules('Last name')}
+          validateFirst
+          hasFeedback
+        >
+          <Input style={{ width: '100%' }} />
+        </Form.Item>
+
+        <Form.Item name="dateOfBirth" label="Date of birth" rules={validation.ageRules}>
+          <DatePicker />
+        </Form.Item>
+
+        <Form.Item name="email" label="Email" rules={validation.emailRules} validateFirst hasFeedback>
+          <Input style={{ width: '100%' }} />
+        </Form.Item>
+
+        <Form.Item name="password" label="Password" rules={validation.passwordRules} validateFirst hasFeedback>
+          <Input.Password style={{ width: '100%' }} />
+        </Form.Item>
+
+        <Form.Item name="country" label="Country" initialValue={countryValue} rules={validation.countryRules}>
+          <Select
+            style={{ width: '100%' }}
+            onChange={(value) => {
+              changeCountry(value);
+            }}
+          >
+            <Select.Option value="Belarus">Belarus</Select.Option>
+            <Select.Option value="Canada">Canada</Select.Option>
+            <Select.Option value="Poland">Poland</Select.Option>
+            <Select.Option value="US">Unated States</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item name="postalCode" label="Postal code" rules={validation.postalCodeRules}>
+          <MaskedInput
+            mask={postalCodeMask}
+            value={postalCodeValue}
+            onChange={(e) => {
+              setPostalCodeValue(e.target.value);
+            }}
+          />
+        </Form.Item>
+
+        <Form.Item name="street" label="Street" rules={validation.streetRules}>
+          <Input style={{ width: '100%' }} />
+        </Form.Item>
+
+        <Form.Item name="city" label="City" rules={validation.textRules('City')}>
+          <Input style={{ width: '100%' }} />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 0, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            CREATE ACCOUNT
+          </Button>
+        </Form.Item>
+      </div>
+    </Form>
   );
 }
