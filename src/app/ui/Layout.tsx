833c9358677c6dbe4c4f9d7cf13922a1255b17b0
Dmitry Markovich <44719809+CrazyTapok@users.noreply.github.com>
2024-05-10 13:05:32 +0300

    Feat/8694gk45n (#16)
    * feat: add layout antd library and connection router-dom and layout
    
    * feat: add footer component and style file for footer
    
    * fix: fix page 404
    
    * fix: delete div in layout component

---
+++ src/app/ui/Layout.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from 'antd';
+
+import { Footer } from '../../components/footer/footer';
+import { Header } from '../../components/header/header';
+import { routes } from '../../router';
+
+import './_layout.scss';
+
+const { Content } = Layout;
+
+export const MyLayout = () => (
+  <Layout className="layout">
+    <Header />
+    <Content className="content">
+      <Routes>
+        {routes.map((rout) => (
+          <Route path={rout.path} element={rout.element} />
+        ))}
+      </Routes>
+    </Content>
+    <Footer />
+  </Layout>
+);
