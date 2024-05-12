d342eae85ef7527c759b01b5265b5f881c2d1075
Luba Hadunova <luba.godunova2018@gmail.com>
2024-05-12 12:27:55 +0200

    feat: change style for login page

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -43,7 +43,7 @@ export function LoginPage() {
 
   return (
     <Form
-      labelCol={{ span: 4 }}
+      labelCol={{ span: 5 }}
       wrapperCol={{ offset: 0, span: 24 }}
       className="login-form"
       onFinish={onFormSubmit}
@@ -53,24 +53,21 @@ export function LoginPage() {
       <div className="login-info">
         <span className="login-title">Sign In</span>
         <Link to={ROUTES.REGISTRATION} className="login-registration">
-          New to Lidilu? Create new account
+          <span className="login-subtitle">New to Lidilu?</span>
+          Create new account
         </Link>
       </div>
       <div className="login-content">
         <Form.Item name="email" label="Email" rules={emailRules} validateFirst hasFeedback>
-          <Input value={email} onChange={(e) => onEmailChange(e.target.value)} style={{ width: '100%' }} />
+          <Input value={email} onChange={(e) => onEmailChange(e.target.value)} />
         </Form.Item>
 
         <Form.Item name="password" label="Password" rules={passwordRules} validateFirst hasFeedback>
-          <Input.Password
-            value={password}
-            onChange={(e) => onPasswordChange(e.target.value)}
-            style={{ width: '100%' }}
-          />
+          <Input.Password value={password} onChange={(e) => onPasswordChange(e.target.value)} />
         </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 0, span: 16 }}>
-          <Button type="primary" htmlType="submit">
+        <Form.Item className="login-footer">
+          <Button className="login-btn" type="primary" htmlType="submit">
             SIGN IN
           </Button>
         </Form.Item>
