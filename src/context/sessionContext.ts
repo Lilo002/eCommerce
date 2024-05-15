d4dcefe26f4b888df02c2a036ee8aaa8fde444f2
Lilo002 <lisabasarab@gmail.com>
2024-05-15 10:30:17 +0300

    refactor: remove auth state from useSession

--- src/context/sessionContext.ts
+++ src/context/sessionContext.ts
@@ -1,13 +1,9 @@
 import { createContext } from 'react';
-import { Project } from '@commercetools/platform-sdk';
 
 import { CustomerDraft, LoginCustomerDraft } from '../sdk/api';
 
-// export const anonymousContext = createContext<{user: Project | null}>({user: null})
 export const sessionContext = createContext<{
   session: {
-    isAuth: boolean;
-    auth: Project | null;
     login: ({ email, password }: LoginCustomerDraft) => Promise<void | Error>;
     logout: () => void;
     isLogin: boolean;
