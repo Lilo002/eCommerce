70378f5977e49d413c0400ffee13d563fdf98d62
Lilo002 <lisabasarab@gmail.com>
2024-05-15 21:35:16 +0300

    feat: add login/registration btns to main page

--- src/pages/main/page.tsx
+++ src/pages/main/page.tsx
@@ -1,9 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'antd';
+
+import { ROUTES } from '../../shared/constants';
+
 import './ui/_main.scss';
 
 export function Main() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate(ROUTES.LOGIN);
+  };
+
+  const handleRegistration = () => {
+    navigate(ROUTES.REGISTRATION);
+  };
+
   return (
     <div className="main">
-      <h2>This will be our main page</h2>
+      <Button className="main-btn" type="link" onClick={handleLogin}>
+        LOG IN
+      </Button>
+      <Button className="main-btn" type="link" onClick={handleRegistration}>
+        SIGN IN
+      </Button>
     </div>
   );
 }
