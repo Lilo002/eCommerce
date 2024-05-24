e6a5c1e4ffee4253be2575142c0bf37c7f221577
Luba Hadunova <luba.godunova2018@gmail.com>
2024-05-24 12:09:52 +0200

    fix: remove modal folder

--- src/pages/product/ui/modal/modal.tsx
+++
@@ -1,9 +0,0 @@
-// import { Modal } from 'antd';
-
-// import { ProductImage } from '../slider/slider';
-
-// const ModalWindow = ({ images }: { images: Image[] }) => (
-//   <Modal open={isModalOpen}>
-//     <ProductImage images={images} />
-//   </Modal>
-// );
